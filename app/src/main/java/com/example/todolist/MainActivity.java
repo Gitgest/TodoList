47864b73c65b902ac7eef74eda3de4ff7cd4e9bd
Gitgest <aable24@gmail.com>
2017-05-14 21:59:46 +0200

    Replaced logging statement in onOptionsItemsSelected()
    Replaced logging statement with functionality allowing the user to write
    a message. Currently the app does nothing with this message.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -29,7 +29,22 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d(TAG, "Add a new Task");
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+
                 return true;
 
             default:
