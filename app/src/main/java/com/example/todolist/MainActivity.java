0f822492b5785b912923b44e0515ac0047e27594
Gitgest <aable24@gmail.com>
2017-05-14 21:26:49 +0200

    Added method onOptionsItemSelected
    Added method onOptionItemSelected to MainActivity.java

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_add_task:
+                Log.d(TAG, "Add a new Task");
+                return true;
+
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
